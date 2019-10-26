2af97f99de489bb711aecfdc6924a38c77eac9c7
sokkyyy <ndegwaraymond@gmail.com>
2019-10-26 13:16:39 +0300

    User and Role models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,29 @@
 from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 
-class User(db.Model):
+
+
+
+class User(UserMixin,db.Model):
     __tablename__='users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
     def __repr__(self):
@@ -14,8 +32,10 @@ class User(db.Model):
 
 class Role(db.Model):
     __tablename__ ='roles'
+    
     id = db.Column(db.Integer,primary_key=True)
-    role = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref='role',lazy='dynamic')
 
     def __repr__(self):
         f'Role {self.role}'
